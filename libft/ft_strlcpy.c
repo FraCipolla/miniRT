0e2a85ef8cf838de719115ed04a6fcb239cc6433
FraCipolla <matcip@hotmail.com>
2022-01-17 19:08:04 +0100

    libft

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,13 @@
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	int	i;
+
+	i = 0;
+	while (dst[i] && src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[dstsize] = '\0';
+	return (dst);
+}
