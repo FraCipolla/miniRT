21033417e833f7337744ad6ec0566a1172574d80
FraCipolla <matcip@hotmail.com>
2022-01-17 19:27:07 +0100

    libft

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,12 @@
+int	strncmp(const char *s1, const char *s2, size_t n)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n)
+	{
+		if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
+	}
+	return (0);
+}
