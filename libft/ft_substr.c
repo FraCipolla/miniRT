34674e30821f20ee4f2f12674c1356b0829142d9
FraCipolla <matcip@hotmail.com>
2022-01-27 16:45:24 +0100

    libft

---
+++ libft/ft_substr.c
@@ -0,0 +1,19 @@
+#include <stlib>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char const		*tab;
+	unsigned int	i;
+
+	i = 0;
+	tab = (char)malloc(sizeof(char) * len));
+	if (!tab)
+		return(NULL);
+	while(i <= len)
+	{
+		tab[i] = s[start];
+		i++;
+		start++;
+	}
+	return (tab);
+}
