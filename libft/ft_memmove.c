b35f984648d0c12793f59f1b2253eb8d02c54cae
FraCipolla <matcip@hotmail.com>
2022-01-27 15:28:21 +0100

    libft

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,11 +1,15 @@
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	int i;
+	int 	i;
+	char	*conv_dst;
+	char	*conv_src;
 
+	conv_src = (char *)src;
+	conv_dst = (char *)dst;
 	i = 0;
 	while (i < len)
 	{
-		dst[i] = src[i];
+		conv_dst[i] = conv_src[i];
 		i++;
 	}
 	return (dst);
