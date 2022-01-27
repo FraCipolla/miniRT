426c6f8ab1a1c53bb030d087dccbc7f08de63331
FraCipolla <matcip@hotmail.com>
2022-01-27 16:35:15 +0100

    libft

---
+++ libft/ft_memcmp.c
@@ -0,0 +1,17 @@
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*conv_s1;
+	unsigned char	*conv_s2;
+
+	i = 0;
+	if (ft_strlen(conv_s1) == 0 && ft_strlen(conv_s2) == 0)
+		return (0);
+	while (i < n && s1[i] == s2[i])
+	{
+		if (conv_s1[i] != conv_s2[i])
+		return (conv_s1[i] - conv_s2[i]);
+		i++;
+	}
+	return (0);
+}
