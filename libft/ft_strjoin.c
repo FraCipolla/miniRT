e3ecce9cb9a52aba5bcea65845ce15213caad1a4
FraCipolla <matcip@hotmail.com>
2022-01-27 17:07:27 +0100

    libft

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,24 @@
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *tab;
+	int		i;
+	int		c;
+
+	i = 0;
+	tab = (char)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
+	if (!tab)
+		return (NULL);
+	while (s1[i])
+	{
+		tab[i] = s1[i];
+		i++
+	}
+	while (s2[c])
+	{
+		tab[i] = s2[c];
+		i++;
+		c++;
+	}
+	return (tab);
+
+}
