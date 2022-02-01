0ddf2377b537eb58f6568e1426be2d24940b1ec3
FraCipolla <matcip@hotmail.com>
2022-02-01 11:34:40 +0100

    libft

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mcipolla <mcipolla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/31 19:45:35 by mcipolla          #+#    #+#             */
-/*   Updated: 2022/02/01 11:11:22 by mcipolla         ###   ########.fr       */
+/*   Updated: 2022/02/01 11:34:38 by mcipolla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@ void	ft_putchar_pf(char c)
 void	ft_print_args (va_list args, t_list *params)
 {
 	if (params->di == 1)
-		ft_print_di(args);
+		ft_print_di(args, params);
 	if (params->u == 1)
 	{
 		params->plus = 0;
-		ft_print_di(args);
+		ft_print_di(args, params);
 	}
 	if (params->x == 1)
-		ft_print_x(args);
+		ft_print_x(args, params);
 	if (params->X == 1)
-		ft_print_X(args);
+		ft_print_X(args, params);
 	if (params->s == 1)
-		ft_print_s(args);
+		ft_print_s(args, params);
 	if (params->c == 1)
-		ft_print_c(args);
+		ft_print_c(args, params);
 	if (params->p == 1)
-		ft_print_p(args);
+		ft_print_p(args, params);
 }
 
 int	ft_printf(const char *format, ...)
@@ -54,12 +54,12 @@ int	ft_printf(const char *format, ...)
 			c = 0;
 			while (format[i] != '\0' || format[i + 1] != '%')
 			{
-				if (ft_check_type(format[i]));
+				if (ft_check_type(format[i], params));
 					break;
 				tmp[c++] = format[i++];
 			}
-			ft_check_params(tmp);
-			ft_print_args (args);
+			ft_check_params(tmp, params);
+			ft_print_args (args, params);
 		}
 		else
 			ft_putchar_pf (format[i]);
