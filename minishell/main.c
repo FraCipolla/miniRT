dfe3bb4cca9e04da9f594663f262c6ab47eb7c58
mcipolla <mcipolla@student.42roma.it>
2022-06-01 18:18:29 +0200

    git

--- minishell/main.c
+++ minishell/main.c
@@ -1,32 +1,17 @@
-#include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <stdlib.h>
 #include <string.h>
-
-static char    *rl_gets (char *str)
-{
-    static char *line;
-  /* If the buffer has already been allocated, return the memory
-     to the free pool. */
-  if (line)
-    {
-      free (line);
-      line = (char *)NULL;
-    }
-
-  /* Get a line from the user. */
-  line = readline ("");
-
-  strcpy(str, line);
-  free (line);
-
-  return (str);
-}
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    char *str;
-    rl_gets(str);
-    return (0);
+	char	*buff;
+
+	buff = readline("minishell");
+	add_history();	
+	printf("%s\n", buff);
+	return (0);
 }
