37116a5f63efc6990cfcd873cce7023c616e2a57
mcipolla <mcipolla@student.42roma.it>
2022-06-01 21:02:39 +0200

    git

--- minishell/main.c
+++ minishell/main.c
@@ -1,17 +1,44 @@
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <string.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcipolla <mcipolla@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/01 19:08:41 by mcipolla          #+#    #+#             */
+/*   Updated: 2022/06/01 21:02:35 by mcipolla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char *argv[])
+#include "minishell.h"
+
+int	check_strcmp(char *str)
+{
+	char	path[1000];
+	
+	if (strcmp(str, "pwd") == 0)
+		{
+			getcwd(path, 100);
+			printf("%s\n", path);
+			return (0);
+		}
+	return (-1);
+}
+
+int main()
 {
 	char	*buff;
 
-	buff = readline("minishell");
-	add_history();	
-	printf("%s\n", buff);
+	while (1)
+	{
+		buff = readline(CGREEN "minishell: " RESET);
+		if (strcmp(buff, "exit") == 0)
+			break ;
+		if (buff != NULL)
+			add_history(buff);
+		if (check_strcmp(buff) == -1)
+			printf("zsh: command not found: %s\n", buff);
+		free(buff);
+	}
 	return (0);
 }
---
+++ minishell/minishell.h
@@ -0,0 +1,16 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <string.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <dirent.h>
+
+# define CGREEN "\033[0;32m"
+# define RESET   "\001\e[0m\002"
+
+#endif
