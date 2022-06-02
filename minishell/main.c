78fac64430e88fa046c4e5da6157638e926f49ab
mcipolla <mcipolla@student.42roma.it>
2022-06-02 18:05:11 +0200

    git

--- minishell/main.c
+++ minishell/main.c
@@ -6,57 +6,85 @@
 /*   By: mcipolla <mcipolla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 19:08:41 by mcipolla          #+#    #+#             */
-/*   Updated: 2022/06/01 22:20:34 by mcipolla         ###   ########.fr       */
+/*   Updated: 2022/06/02 16:19:43 by mcipolla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*command_path(char **envp)
-{
-	while (*envp)
-	{
-		if (ft_strncmp(*envp, "PATH=", 5) == 0)
-			return (*envp + 5);
-		envp++;
-	}
-	return (NULL);
-}
-
 int	check_command(char *str, char **mypath)
 {
-	char	**tmp;
-	char	*cmd;
+	char		**tmp;
+	char		*cmd;
 	extern char **environ;
+	pid_t		pid;
 
-	tmp = ft_split(str, ' ');
-	while (*mypath)
+	pid = fork();
+	if (pid > 0)
 	{
-		cmd = ft_strjoin(*mypath, tmp[0]);
-		if (access(cmd, R_OK) == 0)
-			execve(cmd, tmp, environ);
-		mypath++;
+		waitpid(-1, NULL, 0);
+		return (0);
+	}
+	tmp = ft_split(str, ' ');
+	if (pid == 0)
+	{
+		while (*mypath)
+		{
+			cmd = ft_strjoin(*mypath, tmp[0]);
+			if (access(cmd, R_OK) == 0)
+				execve(cmd, tmp, environ);
+			mypath++;
+		}
+		printf("zsh: command not found: %s\n", str);
+		exit(0);
 	}
 	return (-1);
+}
+
+int	check_pwd(char *str)
+{
+	int	i;
+
+	i = 3;
+	while (str[i])
+	{
+		if (str[i] != ' ')
+		{
+			printf("pwd: too many arguments\n");
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
 }
 
 int	check_strcmp(char *str, char **mypath)
 {
 	char	path[1000];
-	if (strcmp(str, "pwd") == 0)
+	
+	if (strncmp(str, "pwd", 3) == 0)
 	{
-		getcwd(path, 100);
-		printf("%s\n", path);
-		return (0);
+		if (check_pwd(str) == 0)
+		{
+			getcwd(path, 1000);
+			printf("%s\n", path);
+			return (0);
+		}
+		else
+			return (0);
 	}
-	if (strncmp(str, "cd", 2) == 0)
+	else if (strncmp(str, "cd", 2) == 0)
 	{
 		if (str[2] == ' ' && chdir(str + 3) == -1)
 			printf("cd: no such file or directory: %s\n", str + 3);
 		return (0);
 	}
-	if (check_command(str, mypath) == 0)
+	else if (check_command(str, mypath) == 0)
+	{
+		if (strncmp(str, "cat", 3) == 0)
+			write(1, "%\n", 2);
 		return (0);
+	}
 	return (-1);
 }
 
@@ -75,7 +103,7 @@ int main()
 		buff = readline(CGREEN "minishell: " RESET);
 		if (strcmp(buff, "exit") == 0)
 			break ;
-		if (buff != NULL)
+		if (buff != NULL && strncmp(buff, "\0", 1) != 0)
 			add_history(buff);
 		if (check_strcmp(buff, mypath) == -1)
 			printf("zsh: command not found: %s\n", buff);
